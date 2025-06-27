78166d2364d5a84b36d4192948ead846045782d8
Harshit <hc.web1992@gmail.com>
2025-06-27 19:45:11 +0530

    Final changes on Paymnets page

--- src/app/payments.php/page.js
+++ src/app/payments.php/page.js
@@ -32,11 +32,16 @@ export default function Payments() {
         <div className="container">
           <div className="row">
             <div className="col-lg-6 col-md-6 col-sm-12 cntct-box text-center">
-              <Link href="pay-with-paypal.php" className="paypal_btn"><i className="fi fi-brands-paypal"></i> Pay With PAYPAL</Link>
-            </div>
-            <div className="col-lg-6 col-md-6 col-sm-12 cntct-box text-center">
+
               <Link href="https://buy.stripe.com/aFa8wQaMH50j5qY7sd1440p" className="stripe_btn"><i class="fi fi-brands-stripe"></i> Pay With STRIPE</Link>
+              
+              <Link href="pay-with-paypal.php" className="paypal_btn"><i className="fi fi-brands-paypal"></i> Pay With PAYPAL</Link>
+
+              {/* <Link href="https://buy.stripe.com/aFa8wQaMH50j5qY7sd1440p" className="stripe_btn"><i class="fi fi-brands-stripe"></i> Pay With STRIPE</Link> */}
             </div>
+            {/* <div className="col-lg-6 col-md-6 col-sm-12 cntct-box text-center">
+              <Link href="https://buy.stripe.com/aFa8wQaMH50j5qY7sd1440p" className="stripe_btn"><i class="fi fi-brands-stripe"></i> Pay With STRIPE</Link>
+            </div> */}
           </div>
         </div>
       </section>
